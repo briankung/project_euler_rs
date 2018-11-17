4d7f28a065b582824de38f8dceb87f0e13c9c3a0
Brian Kung <brian@callmekung.com>
2018-11-16 22:33:33 -0600

    Incorporates some of /u/oconnor663's advice

--- problem-02/src/main.rs
+++ problem-02/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     let mut current_fibs: Vec<i32> = vec![1, 2];
 
     // Let's count up past 4_000_000
-    let max_size = 4_000_000 as i32;
+    let max_size = 4_000_000;
 
     loop {
         let next_fib = next_fib(&current_fibs);
@@ -13,18 +13,19 @@ fn main() {
         if next_fib > max_size {
             break;
         }
+
         current_fibs.push(next_fib)
     }
 
     // Okay, next to select the even numbers and sum them:
 
-    let sum = current_fibs.iter().filter(|&num| num % 2 == 0).sum::<i32>();
+    let sum: i32 = current_fibs.iter().filter(|&num| num % 2 == 0).sum();
 
-    println!("{:?}", sum);
+    println!("{}", sum);
 }
 
 fn next_fib(fibs: &Vec<i32>) -> i32 {
     let last = fibs.last().unwrap();
-    let second_to_last = fibs.get(fibs.len() - 2).unwrap();
+    let second_to_last = fibs[fibs.len() - 2];
     last + second_to_last
 }
